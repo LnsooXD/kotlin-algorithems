61e4e02426ae2694636c646457c696b0d4f8f819
LnsooXD <LnsooXD@gmail.com>
2022-09-20 16:54:43 +0800

    lowest-common-ancestor-of-a-binary-search-tree

---
+++ src/main/kotlin/leetcode/lowest/common/ancestor/of/a/binary/search/tree/FindPorQ.kt
@@ -0,0 +1,15 @@
+package leetcode.lowest.common.ancestor.of.a.binary.search.tree
+
+import leetcode.struct.TreeNode
+
+class FindPorQ : LowestCommonAncestor {
+    override fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null || root == p || root == q) {
+            return root
+        }
+        val ancestorIsLeft = lowestCommonAncestor(root.left, p, q)
+        val ancestorIsRight = lowestCommonAncestor(root.right, p, q)
+        return if (ancestorIsLeft == null) ancestorIsRight else if (ancestorIsRight == null) ancestorIsLeft else root
+    }
+
+}
