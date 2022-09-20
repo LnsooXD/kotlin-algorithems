99cee84a2636d098615f527665e541d04305e899
LnsooXD <LnsooXD@gmail.com>
2022-09-20 15:49:07 +0800

    ValidateBinarySearchTree

--- src/main/kotlin/leetcode/validate/binary/search/tree/Inorder.kt
+++ src/main/kotlin/leetcode/validate/binary/search/tree/Inorder.kt
@@ -13,12 +13,14 @@ class Inorder : ValidateBinarySearchTree {
         if (!inorderAndValidate(root.left, prev)) { // 执行完之后，prev 会变成 left 中最后一个。
             return false
         }
+
+        // 此时， prev 已经变成 left 中的最后一个
         if (prev.node.let { it != null && it.`val` >= root.`val` }) {
             return false
         }
 
-        prev.node = root
-        return inorderAndValidate(root.right, prev)
+        prev.node = root // right 的前驱是 root 自己
+        return inorderAndValidate(root.right, prev) // prev 变成 right 的最后一个，他是 root 的父节点的前驱
     }
 
     private data class Holder(var node: TreeNode?)
