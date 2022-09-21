1cade393b74848a3f8c1c714490a0322c0fa993a
LnsooXD <LnsooXD@gmail.com>
2022-09-21 23:27:59 +0800

    generate-parentheses balance

--- src/main/kotlin/leetcode/generate/parentheses/Balance.kt
+++ src/main/kotlin/leetcode/generate/parentheses/Balance.kt
@@ -13,9 +13,12 @@ class Balance : GenerateParentheses {
             results.add(result)
             return
         }
+        // 如果 left 还有， 就可以加， 同时也避免了 left < 0 的情况
         if (left > 0) {
             generateParenthesis(results, "$result(", left - 1, right)
         }
+
+        // 如果 right 比 left 多，就可以加。同时因为 left 一定是 >= 0 的， 所以 right 一定大于0
         if (right > left) {
             generateParenthesis(results, "$result)", left, right - 1)
         }
