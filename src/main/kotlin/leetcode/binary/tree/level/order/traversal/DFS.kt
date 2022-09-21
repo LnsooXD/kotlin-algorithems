31a9c1bcc0c398c15bd76759cf4276a76eb6bc57
LnsooXD <LnsooXD@gmail.com>
2022-09-21 21:30:31 +0800

    binary-tree-level-order-traversal dfs

--- src/main/kotlin/leetcode/binary/tree/level/order/traversal/DFS.kt
+++ src/main/kotlin/leetcode/binary/tree/level/order/traversal/DFS.kt
@@ -4,26 +4,19 @@ import leetcode.struct.TreeNode
 import java.util.*
 
 class DFS : BinaryTreeLevelOrderTraversal {
-
-    override fun levelOrder(root: TreeNode?): List<List<Int>> {
-        if (root == null) {
-            return emptyList()
-        }
-        val res = ArrayList<LinkedList<Int>>()
-        this.levelOrder(root, 0, res)
-        return res
+    override fun levelOrder(root: TreeNode?): List<List<Int>> = if (root == null) {
+        emptyList()
+    } else {
+        ArrayList<LinkedList<Int>>().apply { levelOrder(root, 0, this) }
     }
 
     private fun levelOrder(root: TreeNode, level: Int, valuesOfAllLevels: ArrayList<LinkedList<Int>>) {
-        if (level >= valuesOfAllLevels.size) {
-            valuesOfAllLevels.add(LinkedList())
+        val levelValues = if (level >= valuesOfAllLevels.size) {
+            LinkedList<Int>().apply { valuesOfAllLevels.add(this) }
+        } else {
+            valuesOfAllLevels[level]
         }
-        valuesOfAllLevels[level].add(root.`val`)
-
-        if (root.left == null && root.right == null) {
-            return
-        }
-
+        levelValues.add(root.`val`)
         root.left?.let { this.levelOrder(it, level + 1, valuesOfAllLevels) }
         root.right?.let { this.levelOrder(it, level + 1, valuesOfAllLevels) }
     }
