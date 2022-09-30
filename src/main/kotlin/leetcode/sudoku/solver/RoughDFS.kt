a21196ba88849899bac6a2e8e73b23b4aa98be92
LnsooXD <LnsooXD@gmail.com>
2022-09-30 14:53:26 +0800

    sudoku-solver dfs 1/2

---
+++ src/main/kotlin/leetcode/sudoku/solver/RoughDFS.kt
@@ -0,0 +1,45 @@
+package leetcode.sudoku.solver
+
+class RoughDFS : SudokuSolver {
+
+    private val values = CharArray(9) { (it + '1'.toInt()).toChar() }
+
+    override fun solveSudoku(board: Array<CharArray>) {
+        this.solve(board)
+    }
+
+    private fun solve(board: Array<CharArray>): Boolean {
+        for (i in board.indices) for (j in board[0].indices) {
+            if (board[i][j] == '.') {
+                for (value in values) {
+                    if (this.isValid(board, i, j, value)) {
+                        board[i][j] = value
+                        if (this.solve(board)) {
+                            return true
+                        } else {
+                            board[i][j] = '.'
+                        }
+                    }
+                }
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun isValid(board: Array<CharArray>, row: Int, col: Int, value: Char): Boolean {
+        for (i in board.indices) {
+            if (board[row][i] == value) {
+                return false
+            }
+            if (board[i][col] == value) {
+                return false
+            }
+
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value) {
+                return false
+            }
+        }
+        return true
+    }
+}
--- src/test/kotlin/leetcode/sudoku/solver/SudokuSolverTest.kt
+++ src/test/kotlin/leetcode/sudoku/solver/SudokuSolverTest.kt
@@ -11,74 +11,17 @@ internal class SudokuSolverTest {
 
     @Test
     fun testDFS1() {
-        val solver: SudokuSolver = NavieDFS1()
-        val board = arrayOf(
-            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
-            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
-            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
-            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
-            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
-            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
-            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
-            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
-            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
-        )
-        solver.solveSudoku(board)
-        board.forEach {
-            println(it.toList())
-        }
-
-        assertThat(this.checker.isValidSudoku(board)).isTrue()
-
-        assertThat(board).isEqualTo(
-            arrayOf(
-                charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-                charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
-                charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
-                charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
-                charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
-            )
-        )
+        this.test(NavieDFS1())
     }
 
     @Test
     fun testDFS2() {
-        val solver: SudokuSolver = NavieDFS2()
-        val board = arrayOf(
-            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
-            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
-            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
-            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
-            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
-            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
-            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
-            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
-            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
-        )
-        solver.solveSudoku(board)
-        board.forEach {
-            println(it.toList())
-        }
+        this.test(NavieDFS2())
+    }
 
-        assertThat(this.checker.isValidSudoku(board)).isTrue()
-
-        assertThat(board).isEqualTo(
-            arrayOf(
-                charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-                charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
-                charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
-                charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
-                charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
-            )
-        )
+    @Test
+    fun testRoughDFS() {
+        this.test(RoughDFS())
     }
 
     @Test
@@ -89,6 +32,45 @@ internal class SudokuSolverTest {
         board.forEach {
             println(it.toList())
         }
+    }
+
+    private fun test(solver: SudokuSolver) {
+        val board = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+
+        val expected =
+            arrayOf(
+                charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
+                charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
+                charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
+                charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+                charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+                charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+                charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+                charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+                charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
+            )
+
+        this.test(solver, board, expected)
+    }
+
+    private fun test(solver: SudokuSolver, board: Array<CharArray>, expected: Array<CharArray>) {
+        solver.solveSudoku(board)
+        println()
+        board.forEach {
+            println(it.toList())
+        }
+        assertThat(this.checker.isValidSudoku(board)).isTrue()
+        assertThat(board).isEqualTo(expected)
     }
 }
 
