6ab29b79359c0ce205668bad8f23c06fb0ae0f7f
LnsooXD <LnsooXD@gmail.com>
2022-10-10 16:44:18 +0800

    x & -

--- src/test/kotlin/basic/MainTest.kt
+++ src/test/kotlin/basic/MainTest.kt
@@ -10,15 +10,25 @@ class MainTest {
         println(x xor 1)
 
         val y = 1
-        println( y xor 1)
+        println(y xor 1)
     }
 
     @Test
     fun test1() {
-
-        for (x in 0..100) {
-            println("x: $x => ${x.and(-x)}")
+        for (x in -100..100) {
+            println("x: ${x.toUnsignedBinaryString()} => ${x.and(-x)}")
         }
-
     }
+}
+
+fun Int.toUnsignedBinaryString(): String {
+    var mask = 1
+    val res = CharArray(32) { '0' }
+    for (i in 0 until 32) {
+        if (mask.and(this) != 0) {
+            res[31 - i] = '1'
+        }
+        mask = mask.shl(1)
+    }
+    return String(res)
 }
