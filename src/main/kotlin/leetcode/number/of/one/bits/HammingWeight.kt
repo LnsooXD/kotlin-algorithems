4d6eebb0a27cf13e2e26e4e7d137110fc34df37a
LnsooXD <LnsooXD@gmail.com>
2022-10-10 15:19:25 +0800

    number-of-1-bits

--- src/main/kotlin/leetcode/number/of/one/bits/HammingWeight.kt
+++ src/main/kotlin/leetcode/number/of/one/bits/HammingWeight.kt
@@ -5,6 +5,18 @@ package leetcode.number.of.one.bits
 class HammingWeight {
 
     fun hammingWeight(n: Int): Int {
+        var tmp = n
+        var count = 0
+        while (tmp != 0) {
+            if (tmp.and(1) != 0) {
+                count ++
+            }
+            tmp = tmp.ushr(1)
+        }
+        return count
+    }
+
+    fun hammingWeightOld(n: Int): Int {
         var tmp = if (n >= 0) n else n.xor(1.shl(31))
         var count = if (n >= 0) 0 else 1
         while (tmp > 0) {
