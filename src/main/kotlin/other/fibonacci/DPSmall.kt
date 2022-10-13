dda359fa7b0a2791b9b7c8613335fc60f19d6e37
LnsooXD <LnsooXD@gmail.com>
2022-10-13 16:18:42 +0800

    climbing-stairs

--- src/main/kotlin/other/fibonacci/DPSmall.kt
+++ src/main/kotlin/other/fibonacci/DPSmall.kt
@@ -2,15 +2,20 @@ package other.fibonacci
 
 class DPSmall : FibonacciI {
 
+    // 利用4的余数：https://www.jianshu.com/p/0711e9eb8cef
+
+    // 取余运算符为“%”。但在以前，CPU采用如下方法计算余数（注意，该方法只对2的N次方数系有效）：
+    // X & (2^N - 1)
+
     override fun calculate(n: Int): Int {
-        val fibs = IntArray(3) { 0 }
+        val fibs = IntArray(4) { 0 }
         fibs[1] = 1
 
         for (i in 2..n) {
-            fibs[i % 3] = fibs[(i - 1) % 3] + fibs[(i - 2) % 3]
+            fibs[i.and(3)] = fibs[(i - 1).and(3)] + fibs[(i - 2).and(3)]
         }
 
-        return fibs[n % 3]
+        return fibs[n.and(3)]
     }
 
 }
