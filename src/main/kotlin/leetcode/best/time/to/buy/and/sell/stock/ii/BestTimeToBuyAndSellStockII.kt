03264c15597ea011cf27d4a5960eef5cd5f862e6
LnsooXD <LnsooXD@gmail.com>
2022-10-14 17:33:33 +0800

    maximum-product-subarray dp 02

--- src/main/kotlin/leetcode/best/time/to/buy/and/sell/stock/ii/BestTimeToBuyAndSellStockII.kt
+++ src/main/kotlin/leetcode/best/time/to/buy/and/sell/stock/ii/BestTimeToBuyAndSellStockII.kt
@@ -1,5 +1,6 @@
 package leetcode.best.time.to.buy.and.sell.stock.ii
 
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
 interface BestTimeToBuyAndSellStockII {
     fun maxProfit(prices: IntArray): Int
 }
