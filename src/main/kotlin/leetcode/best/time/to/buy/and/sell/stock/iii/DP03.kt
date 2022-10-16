ba80e1bfd21211f3d258bdefb9ab01a3403821a7
LnsooXD <LnsooXD@gmail.com>
2022-10-16 23:31:34 +0800

    123. best-time-to-buy-and-sell-stock-iii dp 3,4

---
+++ src/main/kotlin/leetcode/best/time/to/buy/and/sell/stock/iii/DP03.kt
@@ -0,0 +1,36 @@
+package leetcode.best.time.to.buy.and.sell.stock.iii
+
+class DP03 : BestTimeToBuyAndSellStockIII {
+    override fun maxProfit(prices: IntArray) = this.maxProfit(2, prices)
+
+    private fun maxProfit(k: Int, prices: IntArray): Int {
+        val maxProfits = Array(2) { Array(k + 1) { LongArray(2) { Int.MIN_VALUE.toLong() } } }
+
+        maxProfits[0][0][0] = 0
+        maxProfits[0][0][1] = (-prices[0]).toLong()
+
+        var maxProfit = 0L
+
+        for (i in 1 until prices.size) {
+            val x = (i - 1).and(1)
+            val y = i.and(1)
+            val price = prices[i].toLong()
+
+            maxProfits[y][0][0] = maxProfits[x][0][0]
+            maxProfits[y][0][1] = max(maxProfits[x][0][1], -price)
+
+            for (kk in 1..k) {
+                maxProfits[y][kk][0] = max(maxProfits[x][kk][0], maxProfits[x][kk - 1][1] + price)
+                maxProfits[y][kk][1] = max(maxProfits[x][kk][1], maxProfits[x][kk][0] - price)
+
+                maxProfit = max(maxProfit, maxProfits[y][kk][0])
+            }
+        }
+
+        return maxProfit.toInt()
+    }
+
+    companion object {
+        fun max(a: Long, b: Long) = if (a > b) a else b
+    }
+}
--- src/main/kotlin/leetcode/best/time/to/buy/and/sell/stock/iii/DoublePoint.kt
+++
@@ -1,9 +0,0 @@
-package leetcode.best.time.to.buy.and.sell.stock.iii
-
-class DoublePoint: BestTimeToBuyAndSellStockIII {
-
-    override fun maxProfit(prices: IntArray): Int {
-
-        TODO("Not yet implemented")
-    }
-}
