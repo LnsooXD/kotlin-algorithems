2d42eeb37be2ae43b53de731ed3dde3930288733
LnsooXD <LnsooXD@gmail.com>
2022-10-17 17:37:19 +0800

    322. coin-change dp

--- src/main/kotlin/leetcode/coin/change/DP.kt
+++ src/main/kotlin/leetcode/coin/change/DP.kt
@@ -1,25 +1,25 @@
 package leetcode.coin.change
 
-// counts[i] min coins of i-th step, result is counts[amount]
+// minSteps[i] min coins of i-th step, result is counts[amount]
 
 class DP : CoinChange {
 
     override fun coinChange(coins: IntArray, amount: Int): Int {
-        val counts = IntArray(amount + 1) { Int.MAX_VALUE }
-        counts[0] = 0
+        val minSteps = IntArray(amount + 1) { Int.MAX_VALUE }
+        minSteps[0] = 0
 
         for (i in 1..amount) {
             for (coin in coins) {
                 if (i < coin) {
                     continue
                 }
-                val count = counts[i - coin]
-                if (count < counts[i] - 1) {
-                    counts[i] = count + 1
+                val minStep = minSteps[i - coin]
+                if (minStep < minSteps[i] - 1) {
+                    minSteps[i] = minStep + 1
                 }
             }
         }
-        return if (counts[amount] == Int.MAX_VALUE) -1 else counts[amount]
+        return if (minSteps[amount] == Int.MAX_VALUE) -1 else minSteps[amount]
     }
 
 }
