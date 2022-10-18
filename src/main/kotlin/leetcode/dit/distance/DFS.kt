66b66c4d647b4173558e0b1c3dde2ce58590306c
LnsooXD <LnsooXD@gmail.com>
2022-10-18 09:31:20 +0800

    72. edit-distance dfs

--- src/main/kotlin/leetcode/dit/distance/DFS.kt
+++ src/main/kotlin/leetcode/dit/distance/DFS.kt
@@ -2,9 +2,8 @@ package leetcode.dit.distance
 
 class DFS : EditDistance {
 
-    override fun minDistance(word1: String, word2: String): Int {
-        return minDistance(word1.toCharArray(), 0, word2.toCharArray(), 0)
-    }
+    override fun minDistance(word1: String, word2: String) =
+        minDistance(word1.toCharArray(), 0, word2.toCharArray(), 0)
 
     private fun minDistance(word1: CharArray, index1: Int, word2: CharArray, index2: Int): Int {
         if (index1 >= word1.size || index2 >= word2.size) {
@@ -24,7 +23,7 @@ class DFS : EditDistance {
 
 
     companion object {
-        fun min(a: Int, b: Int) = if (a < b) a else b
+        private fun min(a: Int, b: Int) = if (a < b) a else b
         fun min(a: Int, b: Int, c: Int) = min(min(a, b), c)
         fun max(a: Int, b: Int) = if (a > b) a else b
     }
