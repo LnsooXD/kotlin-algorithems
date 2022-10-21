444f05e4806469f76a0b779ce80e047ef1a96a0f
LnsooXD <LnsooXD@gmail.com>
2022-10-21 16:57:09 +0800

    kth-largest-element-in-a-stream

--- src/test/kotlin/leetcode/KthLargestElementInAStream.kt
+++ src/test/kotlin/leetcode/KthLargestElementInAStream.kt
@@ -6,26 +6,5 @@ import java.util.*
 
 class KthLargestElementInAStream {
 
-    class KthLargest {
 
-        private val k: Int
-        private val queue: Queue<Int>
-
-        constructor(k: Int, nums: IntArray) {
-            this.k = k
-            this.queue = PriorityQueue(k)
-            for (num in nums) {
-                this.add(num)
-            }
-        }
-
-        fun add(`val`: Int): Int {
-            this.queue.offer(`val`)
-            if (this.queue.size > this.k) {
-                this.queue.poll()
-            }
-            return this.queue.peek()
-        }
-
-    }
 }
