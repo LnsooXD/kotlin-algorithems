73d7726f66bb4f6a55f5f4935df669a3825eea9d
LnsooXD <LnsooXD@gmail.com>
2022-10-26 14:07:57 +0800

    120. https://leetcode.com/problems/triangle/ dfs2 simplify

--- src/main/kotlin/leetcode/triangle/DP02.kt
+++ src/main/kotlin/leetcode/triangle/DP02.kt
@@ -4,13 +4,8 @@ package leetcode.triangle
 class DP02 : Triangle {
 
     override fun minimumTotal(triangle: List<List<Int>>): Int {
-        if (triangle.isEmpty()) {
-            return 0
-        }
-        val rows = triangle.size
-        val results = IntArray(rows + 1) { 0 }
-
-        for (row in rows - 1 downTo 0) {
+        val results = IntArray(triangle.size + 1) { 0 }
+        for (row in triangle.size - 1 downTo 0) {
             for (col in 0..row) {
                 results[col] = triangle[row][col] + min(results[col], results[col + 1])
             }
