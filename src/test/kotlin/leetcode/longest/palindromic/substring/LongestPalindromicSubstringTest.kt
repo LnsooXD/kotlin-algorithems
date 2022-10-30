00a381166e1d0c724e86a5df7e9b31ab366abf27
LnsooXD <LnsooXD@gmail.com>
2022-10-30 16:12:34 +0800

    5. https://leetcode.com/problems/longest-palindromic-substring/ test cases

--- src/test/kotlin/leetcode/longest/palindromic/substring/LongestPalindromicSubstringTest.kt
+++ src/test/kotlin/leetcode/longest/palindromic/substring/LongestPalindromicSubstringTest.kt
@@ -7,22 +7,36 @@ internal class LongestPalindromicSubstringTest {
     private val core = CenterCore()
 
     @Test
-    fun test01() {
-        val s = "babad"
+    fun test00() {
+        val s = ""
         val res = this.core.longestPalindrome(s)
-        assertThat(res).isIn("aba", "bab")
+        assertThat(res).isEmpty()
+    }
+
+    @Test
+    fun test01() {
+        val s = "a"
+        val res = this.core.longestPalindrome(s)
+        assertThat(res).isEqualTo("a")
     }
 
     @Test
     fun test02() {
-        val s = "cbbd"
+        val s = "bb"
         val res = this.core.longestPalindrome(s)
         assertThat(res).isIn("bb")
     }
 
     @Test
     fun test03() {
-        val s = "bb"
+        val s = "babad"
+        val res = this.core.longestPalindrome(s)
+        assertThat(res).isIn("aba", "bab")
+    }
+
+    @Test
+    fun test04() {
+        val s = "cbbd"
         val res = this.core.longestPalindrome(s)
         assertThat(res).isIn("bb")
     }
